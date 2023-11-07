f73fd5bd8aa123f9bd25f2166f3ea6f7e337a67f
MistreanuEmanuela <100144278+MistreanuEmanuela@users.noreply.github.com>
2023-11-07 14:59:49 +0200

    Update main.py

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,5 +1,6 @@
 # 1 stack
-from copy import copy
+import copy
+import warnings
 
 print("-------------------------------------``1``---------------------------------------------------")
 
@@ -9,7 +10,8 @@ class Stack:
         self.stack_list = []
 
     def push(self, x):
-        self.stack_list.append(x)
+        value = copy.deepcopy(x)
+        self.stack_list.append(value)
 
     def pop(self):
         if len(self.stack_list) != 0:
@@ -17,14 +19,16 @@ class Stack:
             self.stack_list = self.stack_list[:-1]
             return value
         else:
+            warnings.warn("The stack is empty.", stacklevel=2)
             return None
 
     def peek(self):
-        if len(self.stack_list) != 0:
-            value = copy(self.stack_list[-1])
-            return value
-        else:
+        if self.is_empty():
+            warnings.warn("The stack is empty.", stacklevel=2)
             return None
+        else:
+            value = copy.deepcopy(self.stack_list[-1])
+            return value
 
     def is_empty(self):
         if len(self.stack_list) == 0:
@@ -35,10 +39,10 @@ class Stack:
         return len(self.stack_list)
 
     def __str__(self):
-        queue_str = ""
+        stack_str = ""
         for item in self.stack_list:
-            queue_str += str(item) + " "
-        return queue_str
+            stack_str += str(item) + " "
+        return stack_str
 
 
 my_stack1 = Stack()
@@ -47,23 +51,17 @@ print("Peek stack1:", my_stack1.peek())
 my_stack1.push(1)
 print("stack1", my_stack1)
 print("Peek stack1:", my_stack1.peek())
-my_stack2 = Stack()
-my_stack2.push(5)
-my_stack1.push(2)
-my_stack1.push(3)
-my_stack1.push(4)
-print("stack1", my_stack1)
-print("stack2", my_stack2)
-print("Pop stack1:", my_stack1.pop())
-print("Pop stack1:", my_stack1.pop())
-print("Peek stack2:", my_stack2.peek())
-print("stack1", my_stack1)
-print("stack2", my_stack2)
-print(my_stack2.is_empty())
-print(my_stack2.size())
-my_stack2.pop()
-print("stack2:", my_stack2)
-print(my_stack2.is_empty())
+a = [1, 2, 3]
+my_stack1.push(a)
+a.append(4)
+print("a = ", a)
+print("Peek stack1:", my_stack1.peek())
+b = my_stack1.peek()
+b.append(4)
+print("b =", b)
+print("Peek stack1:", my_stack1.peek())
+print("Is empty:", my_stack1.is_empty())
+print("Size:", my_stack1.size())
 
 
 print("-------------------------------------``2``---------------------------------------------------")
@@ -77,7 +75,8 @@ class Queue:
         self.queue_list = []
 
     def push(self, x):
-        self.queue_list.append(x)
+        value = copy.deepcopy(x)
+        self.queue_list.append(value)
 
     def pop(self):
         if len(self.queue_list) != 0:
@@ -85,14 +84,16 @@ class Queue:
             self.queue_list = self.queue_list[1:]
             return value
         else:
+            warnings.warn("The queue is empty.", stacklevel=2)
             return None
 
     def peek(self):
-        if len(self.queue_list) != 0:
-            value = copy(self.queue_list[0])
-            return value
-        else:
+        if self.is_empty():
+            warnings.warn("The queue is empty.", stacklevel=2)
             return None
+        else:
+            value = copy.deepcopy(self.queue_list[-1])
+            return value
 
     def is_empty(self):
         if len(self.queue_list) == 0:
@@ -118,21 +119,17 @@ peek_q1 = my_q1.peek()
 peek_q1.append(2)
 print(peek_q1)
 print("Peek q1:", my_q1.peek())
-my_q2 = Queue()
-my_q2.push(5)
-my_q1.push(2)
-my_q1.push(3)
-my_q1.push(4)
-print("q1: ", my_q1)
-print("q2: ", my_q2)
-print("Pop q1:", my_q1.pop())
-print("Pop q1:", my_q1.pop())
-print("Peek q2:", my_q2.peek())
-print("q2: ", my_q2)
-print(my_q2.is_empty())
-print(my_q2.size())
-my_q2.pop()
-print(my_q2.is_empty())
+a = [1, 2, 3]
+my_q1.push(a)
+a.append(4)
+print("a= ", a)
+print("queue: ", my_q1)
+
+
+print("Is empty:", my_q1.is_empty())
+print("Size(): ", my_q1.size())
+print(my_q1.pop())
+
 
 print("-------------------------------------``3``---------------------------------------------------")
 
@@ -142,13 +139,14 @@ class Matrix:
         if n > 0 and m > 0:
             self.rows = n
             self.columns = m
-            self.matrix = [[0 for _ in range(m)] for _ in range(n)]
+            self.matrix = [[None for _ in range(m)] for _ in range(n)]
         else:
             raise Exception("matrix should have a positive size")
 
     def set(self, i, j, x):
+        value = copy.deepcopy(x)
         if 0 <= i < self.rows and 0 <= j < self.columns:
-            self.matrix[i][j] = x
+            self.matrix[i][j] = value
         elif i < 0 or i >= self.rows:
             raise Exception(f"index i = {i} out of bounds")
         elif j < 0 or j >= self.columns:
@@ -156,7 +154,8 @@ class Matrix:
 
     def get(self, i, j):
         if 0 <= i < self.rows and 0 <= j < self.columns:
-            return self.matrix[i][j]
+            value = copy.deepcopy(self.matrix[i][j])
+            return value
         elif i < 0 or i >= self.rows:
             raise Exception(f"index i = {i} out of bounds")
         elif j < 0 or j >= self.columns:
@@ -164,9 +163,10 @@ class Matrix:
 
     def transpose(self):
         new_matrix = [[0 for _ in range(self.rows)] for _ in range(self.columns)]
+        copy_matrix = copy.deepcopy(self.matrix)
         for i in range(self.columns):
             for j in range(self.rows):
-                new_matrix[i][j] = self.matrix[j][i]
+                new_matrix[i][j] = copy_matrix[j][i]
         return new_matrix
 
     def multiply(self, other_matrix):
@@ -176,19 +176,41 @@ class Matrix:
         for i in range(self.rows):
             for j in range(self.columns):
                 for k in range(other_matrix.columns):
-                    multiply_matrix[i][j] += self.matrix[i][k] * other_matrix.matrix[k][j]
+                    try:
+                        multiply_matrix[i][j] += self.matrix[i][k] * other_matrix.matrix[k][j]
+                    except Exception as e:
+                        raise Exception(f"An error occurred during matrix multiplication: {e}")
         return multiply_matrix
 
     def transformation(self, lambda_funct):
+        matrix_Copy = copy.deepcopy(self.matrix)
+        errors = False
         for i in range(self.rows):
             for j in range(self.columns):
-                self.matrix[i][j] = lambda_funct(self.matrix[i][j])
+                try:
+                    matrix_Copy[i][j] = lambda_funct(self.matrix[i][j])
+                except Exception as e:
+                    errors = True
+                    raise Exception(f"An error occurred during transformation: {e}")
+        if not errors:
+            self.matrix = copy.deepcopy(matrix_Copy)
 
     def __str__(self):
         matrix_str = ""
         for i in range(self.rows):
             matrix_str += str(self.matrix[i]) + "\n"
         return matrix_str
+
+
+matrix_test = Matrix(1, 1)
+a = [1, 1]
+matrix_test.set(0, 0, a)
+a.append(2)
+print(matrix_test)
+b = matrix_test.get(0, 0)
+b.append(5)
+print(matrix_test)
+
 
 
 matrix = Matrix(2, 3)
@@ -223,7 +245,3 @@ print(matrix2)
 matrix_p = matrix.multiply(matrix2)
 print("Matrix Result:")
 print(matrix_p)
-
-matrix.transformation(lambda x: x-2)
-print("Transformed Matrix:")
-print(matrix)
